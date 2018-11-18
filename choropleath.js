181220793f71b178d8447851cc7f2c62e860f835
Sahana Balram <sahana2008balram@gmail.com>
2018-11-18 09:28:08 -0800

    define body

--- choropleath.js
+++ choropleath.js
@@ -1,3 +1,10 @@
 $(document).ready(function(){
-    
+    var body = d3.select("body");
+    // define body
+    var svg = d3.select("svg"),
+        width = +svg.attr("width"),
+        height = +svg.attr("height");
+
+        // define svg
+        
 });
