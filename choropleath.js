594e0752e0933dd71291926d5873d65a6b9a0044
Sahana Balram <sahana2008balram@gmail.com>
2018-11-18 09:44:06 -0800

    inculde x axis, color and tooltip

--- choropleath.js
+++ choropleath.js
@@ -5,6 +5,21 @@ $(document).ready(function(){
         width = +svg.attr("width"),
         height = +svg.attr("height");
 
-        // define svg
+        // define tooltip
+        var tooltip = body.append("div")
+            .attr("class", "tooltip")
+            .attr("id", "tooltip")
+            .style("opacity",0);
+
+        var unemployment = d3.map();
+        var path = d3.geopath();
+
+        var x = d3.scaleLiner()
+            .domain([2.6, 75.1])
+            .rangeRound([600,860]);
         
+        var color = d3.scaleThreshold()
+            .domain(d3.range(2.6,75.1,(75.1-2.6)/8))
+            .range(d3.schemeGreens[9]);
+
 });
